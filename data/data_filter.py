2dc86c815ad1004e07d6e3148baf76dcbde7e3ec
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-04-30 11:22:26 +0200

    Python-Skript kann jetzt plotten

--- data/data_filter.py
+++ data/data_filter.py
@@ -3,4 +3,20 @@ from scipy.io import arff
 data, meta = arff.loadarff("TRIPID 360933 Data direkt ROT-HAM.arff")
 print "Data finished loading."
 
-print data["time"][0:5]
+tripids = data["TripID"]
+longitudes = data["Longitude"]
+latitudes = data["Latitude"]
+
+filteredLong = []
+filteredlat = []
+l = len(tripids)
+for i in range(0, l):
+    if tripids[i] == 360933:
+        filteredLong.append(longitudes[i])
+        filteredlat.append(latitudes[i])
+
+import matplotlib.pyplot as plt
+
+plt.plot(filteredLong, filteredlat, "ro")
+plt.axis([2.89, 10.0, 51.68, 54.54])
+plt.show()

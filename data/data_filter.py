de03f14f217de320faf03ac952fe78bda9eef552
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-04-30 15:41:20 +0200

    Versuch mit Python die Routen in eine Karte zu malen

--- data/data_filter.py
+++ data/data_filter.py
@@ -2,10 +2,23 @@ import pandas
 
 dataset = pandas.read_csv("PY11 rotterdam_hamburg - times to UNIX time.csv")
 
+from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 
 xs = dataset["Longitude"]
 ys = dataset["Latitude"]
 
-plt.plot(xs, ys, "x")
-plt.show()
+print dataset
+
+"""
+map = Basemap(projection='mill',resolution="h", llcrnrlon=2, llcrnrlat=51, urcrnrlon=11, urcrnrlat=55.5)
+map.drawcoastlines()
+map.drawcountries()
+map.drawrivers()
+map.fillcontinents(color='tan',lake_color='lightblue')
+map.drawmapboundary(fill_color='lightblue')
+
+map.scatter(xs, ys, latlong="True", marker="D", color="m")
+"""
+
+#plt.show()

aabe776d2e286373ef52dffbff249b8187e68891
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-06-18 11:29:59 +0200

    Pseudocode für Broker geschrieben

--- App/broker.py
+++ App/broker.py
@@ -1,1 +1,10 @@
 a = 42 + 324
+
+# Gets trip lines (arff)
+# Take out values I care about (learned on)
+# while current position isn't destination
+#     Determine agent for current position
+#     Send values to agent
+#     Receive new values at sector leave from agent (e.g. leave latitude, leave longitude, time to leave)
+#     Add sector travel time to entire travel time
+# return entire travel time and route

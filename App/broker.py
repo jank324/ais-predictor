72cce45d1083e5979c7569c25b3c3af25b056c7b
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-06-18 16:13:32 +0200

    Anfrage wird jetzt vom Broker bearbeitet

--- App/broker.py
+++ App/broker.py
@@ -14,25 +14,22 @@ import sys
 
 learners = ['Latitude', 'Longitude']
 
-file_path = sys.argv[1]
-print('The file path is \"%s\"' % (file_path))
-
-names = ['TripID', 'MMSI', 'StartLatitude', 'StartLongitude', 'StartTime', 'EndLatitude', 'EndLongitude', 'EndTime',
+def extract_data(file_path):
+    '''
+    names = ['TripID', 'MMSI', 'StartLatitude', 'StartLongitude', 'StartTime', 'EndLatitude', 'EndLongitude', 'EndTime',
          'StartPort', 'EndPort', 'ID', 'time', 'shiptype', 'Length', 'Breadth', 'Draught', 'Latitude', 'Longitude',
          'SOG', 'COG', 'TH', 'Destination', 'Name', 'Callsign', 'AisSourcen']
-df = pd.read_csv(file_path, names = names, skiprows = 27, parse_dates = True,
+    df = pd.read_csv(file_path, names = names, skiprows = 27, parse_dates = True,
                  na_values = ['?'], dtype = {'TripID': str, 'MMSI': str, 'shiptype': str})
 
-df['time'] = pd.to_datetime(df['time'], format = '\'%Y-%m-%d %H:%M\'')
+    df['time'] = pd.to_datetime(df['time'], format = '\'%Y-%m-%d %H:%M\'')
 
-df.loc[df['TH'] >= 360, 'TH'] = np.nan
-df.loc[df['COG'] >= 360, 'COG'] = np.nan
-df.loc[(df['Length'] <= 0) | (df['Length'] > 400), 'Length'] = np.nan
-df.loc[(df['Breadth'] <= 0) | (df['Breadth'] > 59), 'Breadth'] = np.nan
-df.loc[df['SOG'] > 25.6, 'SOG'] = np.nan
+    df.loc[df['TH'] >= 360, 'TH'] = np.nan
+    df.loc[df['COG'] >= 360, 'COG'] = np.nan
+    df.loc[(df['Length'] <= 0) | (df['Length'] > 400), 'Length'] = np.nan
+    df.loc[(df['Breadth'] <= 0) | (df['Breadth'] > 59), 'Breadth'] = np.nan
+    df.loc[df['SOG'] > 25.6, 'SOG'] = np.nan
 
-df = df[learners]
-
-for index, row in df.iterrows():
-    print("Broker calling agent ...")
-    os.system("python example_agent.py %f %f" % (row['Latitude'], row['Longitude']))
+    return df[learners]
+    '''
+    return "%d" % (file_path * 2)

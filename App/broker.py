ea4ecafc8069c8e1b9f7f00ff304757349a9a59c
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-06-18 13:13:17 +0200

    erste Datenextraktion im Broker implementiert

--- App/broker.py
+++ App/broker.py
@@ -7,5 +7,44 @@
 #     Add sector travel time to entire travel time
 # return entire travel time and route
 
-def broker():
-    return 324
+import numpy as np
+import pandas as pd
+import sys
+
+file_path = sys.argv[1]
+print('The file path is \"%s\"' % (file_path))
+
+# Load the data
+names = ['TripID', 'MMSI', 'StartLatitude', 'StartLongitude', 'StartTime', 'EndLatitude', 'EndLongitude', 'EndTime',
+         'StartPort', 'EndPort', 'ID', 'time', 'shiptype', 'Length', 'Breadth', 'Draught', 'Latitude', 'Longitude',
+         'SOG', 'COG', 'TH', 'Destination', 'Name', 'Callsign', 'AisSourcen']
+df = pd.read_csv(file_path, names = names, skiprows = 27, parse_dates = True,
+                 na_values = ['?'], dtype = {'TripID': str, 'MMSI': str, 'shiptype': str})
+
+# Convert time columns to correct dtype
+df['StartTime'] = pd.to_datetime(df['StartTime'], format = '\'%Y-%m-%d %H:%M\'')
+df['EndTime'] = pd.to_datetime(df['EndTime'], format = '\'%Y-%m-%d %H:%M\'')
+df['time'] = pd.to_datetime(df['time'], format = '\'%Y-%m-%d %H:%M\'')
+
+# Convert all headings that are 511 (>= 360) to NaN
+df.loc[df['TH'] >= 360, 'TH'] = np.nan
+
+# Convert courses >= 360 to NaN
+df.loc[df['COG'] >= 360, 'COG'] = np.nan
+
+# Set invalid shiptypes to NaN
+# Invalid shiptypes existing in the data set are '0' and '159'
+df.loc[(df['shiptype'] == '0') | (df['shiptype'] == '159'), 'shiptype'] = np.nan
+
+# Set invalid lengths (0 or > 400) to NaN
+df.loc[(df['Length'] <= 0) | (df['Length'] > 400), 'Length'] = np.nan
+
+# Set invalid breadths (0 or > 59) to NaN
+df.loc[(df['Breadth'] <= 0) | (df['Breadth'] > 59), 'Breadth'] = np.nan
+
+# Set speeds that are unrealisticly high to NaN
+df.loc[df['SOG'] > 25.6, 'SOG'] = np.nan
+
+df = df.drop(['AisSourcen', 'ID', 'StartPort', 'EndPort', 'Name', 'MMSI', 'shiptype'], axis = 1)
+
+print(df)

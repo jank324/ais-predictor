f7ff1371ddd352a9aa9105f0780840c3c35546da
Vincent Hanke <vincent.hanke@googlemail.com>
2018-06-20 18:07:39 +0200

    Webseite nimmt arff Datei und gibt sie geplottet aus

--- App/testServer.py
+++ App/testServer.py
@@ -2,19 +2,25 @@ from flask import Flask, request, render_template
 import subprocess
 import sys
 from werkzeug.utils import secure_filename
-app = Flask(__name__)
+import os
+
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def my_form():
     return render_template('test.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def my_form_post():
     f = request.files['file']
-    f.save(secure_filename(f.filename))
+    name = './static/'
+    name += f.filename
+    f.save(name)
     text = './testBroker.py'
-    output = subprocess.check_output([sys.executable, text, f.filename])
-    return render_template("test.html",output = output)
+    output = subprocess.check_output([sys.executable, text, name])
+    #file_name = os.path.join(pictures, f.filename)
+    output = './static/pic.jpg'
+    return render_template("test.html", output = output)
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
